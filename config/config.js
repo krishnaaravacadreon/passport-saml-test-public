a04bc41535666c8fac1492a92d8245be4991e1e7
Antoine Drian <antoine.drian@vesperiagroup.com>
2017-05-30 17:10:55 +0200

    forgot https

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = {
       strategy: 'saml',
       saml: {
         path: process.env.SAML_PATH || '/login/callback',
-        entryPoint: process.env.SAML_ENTRY_POINT || 'https://idp.ssocircle.com/sso/idpssoinit?metaAlias=%2Fpublicidp&spEntityID=http://serveur.network-drian.ovh:8083/metadata',//'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+        entryPoint: process.env.SAML_ENTRY_POINT || 'https://idp.ssocircle.com/sso/idpssoinit?metaAlias=%2Fpublicidp&spEntityID=https://serveur.network-drian.ovh:8083/metadata',//'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
         //callbackUrl: 'http://serveur.network-drian.ovh:8083/login/callback/',
         issuer: 'http://serveur.network-drian.ovh:8083', //'passport-saml',
         cert: process.env.SAML_CERT || fs.readFileSync( certif , { encoding: 'utf8' })
